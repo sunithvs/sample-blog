4546ebfac4d673c8bb24d9a716d0eeb18bb5ec77
sunith vs <sunithvs2002@gmail.com>
2025-12-26 21:48:53 +0530

    feat: Add Next.js rewrites to proxy blog subdomains.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/blogs/:slug*",
+        destination: "https://blogs.ottamind.in/blogs/:slug*",
+      },
+      {
+        source: "/:match*",
+        destination: "https://blog.ottamind.in/:match*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
