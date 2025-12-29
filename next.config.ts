1a053bda67daa2bfb34ad8926be6217d326a9d1d
sunith vs <63339782+sunithvs@users.noreply.github.com>
2025-12-29 14:18:14 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,27 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/blogs/:slug*",
-        destination: "https://blog.ottamind.in/blogs/:slug*",
+        // This handles the main blog page (ottamind.in)
+        source: "/blog",
+        destination: "https://blog.ottamind.in",
+      },
+      {
+        // This handles posts (e.g., ottamind.in/privacy-policy)
+        source: "/blog/:path*",
+        destination: "blog.ottamind.in*",
+      },
+      {
+        // This handles the CSS/JS files so the UI looks correct
+        source: "/wp-content/:path*",
+        destination: "blog.ottamind.in*",
+      },
+      {
+        // This handles WordPress core system files
+        source: "/wp-includes/:path*",
+        destination: "blog.ottamind.in*",
       }
     ];
   },
 };
+
+export default nextConfig;
