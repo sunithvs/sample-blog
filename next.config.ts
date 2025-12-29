bfb1e5fb67de418862267ec854cff1fa16d9203a
sunith vs <63339782+sunithvs@users.noreply.github.com>
2025-12-29 13:56:54 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/blogs/:slug*",
-        destination: "https://blogs.ottamind.in/:slug*",
-      },
-      {
-        source: "/:match*",
-        destination: "https://blog.ottamind.in/:match*",
-      },
+        destination: "https://blog.ottamind.in/:slug*",
+      }
     ];
   },
 };
